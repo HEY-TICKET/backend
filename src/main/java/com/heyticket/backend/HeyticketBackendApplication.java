ae6a09ce1974c596bb1e832d933b671c04538814
whlee77 <whlee@posicube.com>
2023-05-01 23:21:13 +0900

    initial commit

---
+++ src/main/java/com/heyticket/backend/HeyticketBackendApplication.java
@@ -0,0 +1,13 @@
+package com.heyticket.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeyticketBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HeyticketBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/heyticket/backend/HeyticketBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heyticket.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeyticketBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
