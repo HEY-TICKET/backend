bdcd804c79f83b7f69040963b50b50e080cf5a6c
wonhee77 <leehi402@naver.com>
2023-05-05 02:39:12 +0900

    feat: Genre enum 추가

---
+++ src/main/java/com/heyticket/backend/kopis/enums/Genre.java
@@ -0,0 +1,26 @@
+package com.heyticket.backend.kopis.enums;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum Genre {
+
+    THEATER("AAAA", "연극", 1),
+    DANCE("BBBC", "무용", 2),
+    CONTEMPORARY_DANCE("BBBE", "대중무용", 3),
+    CLASSIC("CCCA", "클래식", 4),
+    KOREAN_TRADITIONAL_MUSIC("CCCC", "국악", 5),
+    POPULAR_MUSIC("CCCD", "대중음악", 6),
+    MIXED_GENRE("EEEA", "복합", 7),
+    CIRCUS_AND_MAGIC("EEEB", "서커스/마술", 8),
+    MUSICAL("GGGA", "뮤지컬", 9);
+
+    private final String code;
+
+    private final String name;
+
+    private final int number;
+
+}
