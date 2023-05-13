18e2dc3eb3d5d117491af3a53af47ffed90f0311
wonhee77 <leehi402@naver.com>
2023-05-13 14:30:51 +0900

    feat: swagger 추가

---
+++ src/main/java/com/heyticket/backend/controller/SwaggerController.java
@@ -0,0 +1,24 @@
+package com.heyticket.backend.controller;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import java.net.URI;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+@Tag(name = "swagger-controller", description = "Only for Swagger redirect")
+public class SwaggerController {
+
+    @GetMapping("/swagger")
+    public ResponseEntity<Void> swagger() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setLocation(URI.create("/swagger-ui/index.html"));
+        return new ResponseEntity<>(headers, HttpStatus.MOVED_PERMANENTLY);
+    }
+
+}
