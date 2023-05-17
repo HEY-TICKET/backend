3853452c78c06be0cc8a40197dd73dd368d8d5a1
wonhee77 <leehi402@naver.com>
2023-05-17 22:52:57 +0900

    feat: spring security 추가, jwt 인증 로직 추가

---
+++ src/main/java/com/heyticket/backend/config/SecurityConfig.java
@@ -0,0 +1,44 @@
+package com.heyticket.backend.config;
+
+import com.heyticket.backend.module.security.jwt.JwtAuthenticationFilter;
+import com.heyticket.backend.module.security.jwt.JwtTokenProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final JwtTokenProvider jwtTokenProvider;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf().disable()
+            .cors().disable()
+            .httpBasic().disable()
+            .formLogin().disable()
+            .authorizeHttpRequests(authorize -> authorize.anyRequest().authenticated())
+            .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Bean
+    public WebSecurityCustomizer configure() {
+        return (web) -> web.ignoring().requestMatchers("/api/members/login");
+    }
+}
--- src/test/java/com/heyticket/backend/service/PerformanceServiceTest.java
+++ src/test/java/com/heyticket/backend/service/PerformanceServiceTest.java
@@ -1,30 +1,30 @@
-package com.heyticket.backend.service;
-
-import java.time.LocalDate;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class PerformanceServiceTest {
-
-    @Autowired
-    private PerformanceService performanceService;
-
-    @Test
-    void savePerformance() {
-        performanceService.updatePerformances(LocalDate.now().minusMonths(1), LocalDate.now().plusMonths(12), 3000);
-    }
-
-    @Test
-    void getUniBoxOffice() {
-//        List<BoxOfficeResponse> uniBoxOffice = performanceService.getUniBoxOffice();
-//        System.out.println("uniBoxOffice.size() = " + uniBoxOffice.size());
-    }
-
-    @Test
-    void updateBoxOfficeRank() {
-        performanceService.updateBoxOfficeRank();
-    }
-
-}
+//package com.heyticket.backend.service;
+//
+//import java.time.LocalDate;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class PerformanceServiceTest {
+//
+//    @Autowired
+//    private PerformanceService performanceService;
+//
+//    @Test
+//    void savePerformance() {
+//        performanceService.updatePerformances(LocalDate.now().minusMonths(1), LocalDate.now().plusMonths(12), 3000);
+//    }
+//
+//    @Test
+//    void getUniBoxOffice() {
+////        List<BoxOfficeResponse> uniBoxOffice = performanceService.getUniBoxOffice();
+////        System.out.println("uniBoxOffice.size() = " + uniBoxOffice.size());
+//    }
+//
+//    @Test
+//    void updateBoxOfficeRank() {
+//        performanceService.updateBoxOfficeRank();
+//    }
+//
+//}
