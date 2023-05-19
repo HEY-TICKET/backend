615f1bb3ceff06afdaab579d1409f74f7573763f
wonhee77 <leehi402@naver.com>
2023-05-20 00:01:05 +0900

    fix: SMTP 메일 형식 변경 bean에 생성되는 변수 제거, 동시성 이슈 처리

--- src/main/java/com/heyticket/backend/service/MailService.java
+++ src/main/java/com/heyticket/backend/service/MailService.java
@@ -13,71 +13,13 @@ import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class MailService  {
+public class MailService {
 
     private final JavaMailSender emailSender;
 
-    private String ePw; // 인증번호
-
-    public MimeMessage createMessage(String to) throws MessagingException, UnsupportedEncodingException {
-        MimeMessage message = emailSender.createMimeMessage();
-
-        message.addRecipients(RecipientType.TO, to);// 보내는 대상
-        message.setSubject("Hey-ticket 회원가입 이메일 인증");// 제목
-
-        StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append("<div style='margin:100px;'>");
-        stringBuilder.append("<h1> 안녕하세요</h1>");
-        stringBuilder.append("<br>");
-        stringBuilder.append("<div align='center' style='border:1px solid black; font-family:verdana';>");
-        stringBuilder.append("<h3 style='color:blue;'>회원가입 인증 코드입니다.</h3>");
-        stringBuilder.append("<div style='font-size:130%'>");
-        stringBuilder.append("CODE : <strong>");
-        stringBuilder.append(ePw).append("</strong><div><br/> "); // 메일에 인증번호 넣기)
-        stringBuilder.append("</div>");
-        message.setText(stringBuilder.toString(), "utf-8", "html");// 내용, charset 타입, subtype
-        // 보내는 사람의 이메일 주소, 보내는 사람 이름
-        message.setFrom(new InternetAddress("goodjobproject@naver.com", "GoodJob_Admin"));// 보내는 사람
-
-        return message;
-    }
-
-    // 랜덤 인증 코드 전송
-    public String createKey() {
-        StringBuffer key = new StringBuffer();
-        Random rnd = new Random();
-
-        for (int i = 0; i < 8; i++) { // 인증코드 8자리
-            int index = rnd.nextInt(3); // 0~2 까지 랜덤, rnd 값에 따라서 아래 switch 문이 실행됨
-
-            switch (index) {
-                case 0:
-                    key.append((char) ((int) (rnd.nextInt(26)) + 97));
-                    // a~z (ex. 1+97=98 => (char)98 = 'b')
-                    break;
-                case 1:
-                    key.append((char) ((int) (rnd.nextInt(26)) + 65));
-                    // A~Z
-                    break;
-                case 2:
-                    key.append((rnd.nextInt(10)));
-                    // 0~9
-                    break;
-            }
-        }
-
-        return key.toString();
-    }
-
-    // 메일 발송
-    // sendSimpleMessage 의 매개변수로 들어온 to 는 곧 이메일 주소가 되고,
-    // MimeMessage 객체 안에 내가 전송할 메일의 내용을 담는다.
     public String sendSimpleMessage(String to) throws Exception {
-
-        ePw = createKey(); // 랜덤 인증번호 생성
-
-        // TODO Auto-generated method stub
-        MimeMessage message = createMessage(to); // 메일 발송
+        String key = createKey();// 랜덤 인증번호 생성
+        MimeMessage message = createMessage(key, to); // 메일 발송
         try {// 예외처리
             emailSender.send(message);
         } catch (MailException es) {
@@ -85,8 +27,44 @@ public class MailService  {
             throw new IllegalArgumentException();
         }
 
-
-        return ePw; // 메일로 보냈던 인증 코드를 서버로 반환
+        return key; // 메일로 보냈던 인증 코드를 서버로 반환
     }
 
+    public MimeMessage createMessage(String key, String to) throws MessagingException, UnsupportedEncodingException {
+        MimeMessage message = emailSender.createMimeMessage();
+        message.addRecipients(RecipientType.TO, to);
+        message.setSubject("[헤이티켓] 회원 가입 인증 메일입니다.");
+
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("<div style='margin:100px;'>");
+        stringBuilder.append("<h1 align='center'> 안녕하세요</h1>");
+        stringBuilder.append("<br>");
+        stringBuilder.append("<div align='center' width='400px' style='border:1px solid black; font-family:verdana';>");
+        stringBuilder.append("<h3 style='color:blue;'>회원가입 인증 코드입니다.</h3>");
+        stringBuilder.append("<div style='font-size:130%'>");
+        stringBuilder.append("CODE : <strong>");
+        stringBuilder.append(key).append("</strong><div><br/> ");
+        stringBuilder.append("</div>");
+        message.setText(stringBuilder.toString(), "utf-8", "html");
+        message.setFrom(new InternetAddress("heyticket@gmail.com", "헤이티켓"));
+
+        return message;
+    }
+
+    private String createKey() {
+        StringBuffer key = new StringBuffer();
+        Random rnd = new Random();
+
+        for (int i = 0; i < 8; i++) { // 인증코드 8자리
+            int index = rnd.nextInt(3); // 0~2 까지 랜덤, rnd 값에 따라서 아래 switch 문이 실행됨
+
+            switch (index) {
+                case 0 -> key.append((char) (rnd.nextInt(26) + 97)); // a~z (ex. 1+97=98 => (char)98 = 'b')
+                case 1 -> key.append((char) (rnd.nextInt(26) + 65)); // A~Z
+                case 2 -> key.append((rnd.nextInt(10))); // 0~9
+            }
+        }
+
+        return key.toString();
+    }
 }
