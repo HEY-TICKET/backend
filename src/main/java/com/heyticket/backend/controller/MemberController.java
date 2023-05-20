aedb7f6093dd279f85f6d00a18a5be77d76de555
wonhee77 <leehi402@naver.com>
2023-05-20 20:08:16 +0900

    feat: 회원가입, email 전송, 검증, 인증코드 만료 api 추가

--- src/main/java/com/heyticket/backend/controller/MemberController.java
+++ src/main/java/com/heyticket/backend/controller/MemberController.java
@@ -1,10 +1,14 @@
 package com.heyticket.backend.controller;
 
 import com.heyticket.backend.module.security.jwt.dto.TokenInfo;
+import com.heyticket.backend.service.EmailService;
 import com.heyticket.backend.service.MemberService;
+import com.heyticket.backend.service.dto.EmailVerificationRequest;
 import com.heyticket.backend.service.dto.MemberLoginRequest;
+import com.heyticket.backend.service.dto.MemberSignUpRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,11 +22,30 @@ public class MemberController {
 
     private final MemberService memberService;
 
+    private final EmailService emailService;
+
     @PostMapping("/members/login")
-    public TokenInfo login(@RequestBody MemberLoginRequest memberLoginRequest) {
-        String memberId = memberLoginRequest.getMemberId();
-        String password = memberLoginRequest.getPassword();
-        TokenInfo tokenInfo = memberService.login(memberId, password);
-        return tokenInfo;
+    public TokenInfo login(@RequestBody MemberLoginRequest request) {
+        return memberService.login(request);
+    }
+
+    @PostMapping("/members/signup")
+    public void signUp(@RequestBody MemberSignUpRequest request) {
+        memberService.signUp(request);
+    }
+
+    @PostMapping("/members/email/send")
+    public void sendVerificationEmail(@RequestBody EmailVerificationRequest request) {
+        emailService.sendSimpleMessage(request);
+    }
+
+    @PostMapping("/members/email/verify")
+    public boolean verifyEmail(@RequestBody EmailVerificationRequest request) {
+        return emailService.validateEmail(request);
+    }
+
+    @DeleteMapping("/members/email/expire")
+    public void expireVerificationCode(EmailVerificationRequest request) {
+        emailService.expireCode(request.getEmail());
     }
 }
--- src/main/java/com/heyticket/backend/service/MemberService.java
+++ src/main/java/com/heyticket/backend/service/MemberService.java
@@ -1,13 +1,20 @@
 package com.heyticket.backend.service;
 
+import com.heyticket.backend.domain.Member;
 import com.heyticket.backend.module.security.jwt.JwtTokenProvider;
 import com.heyticket.backend.module.security.jwt.dto.TokenInfo;
+import com.heyticket.backend.module.util.PasswordValidator;
 import com.heyticket.backend.repository.MemberRepository;
+import com.heyticket.backend.service.dto.MemberLoginRequest;
+import com.heyticket.backend.service.dto.MemberSignUpRequest;
 import jakarta.transaction.Transactional;
+import java.security.InvalidParameterException;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -21,14 +28,48 @@ public class MemberService {
 
     private final JwtTokenProvider jwtTokenProvider;
 
-    public void register(String memberId, String password) {
+    private final PasswordEncoder passwordEncoder;
 
+    private final CacheService cacheService;
+
+    public void signUp(MemberSignUpRequest request) {
+        String email = request.getEmail();
+        String password = request.getPassword();
+
+        PasswordValidator.validatePassword(password);
+        checkIfExistingMember(email);
+        validateCode(email, request.getVerificationCode());
+
+        Member member = Member.builder()
+            .email(email)
+            .password(passwordEncoder.encode(password))
+            .roles(List.of("USER"))
+            .build();
+
+        memberRepository.save(member);
     }
 
-    public TokenInfo login(String memberId, String password) {
-        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(memberId, password);
+    private void validateCode(String email, String code) {
+        String savedCode = cacheService.getIfPresent(email);
+        if (savedCode == null) {
+            throw new IllegalStateException("invalid verification email");
+        }
+        if (!savedCode.equals(code)) {
+            throw new IllegalStateException("fail to verification");
+        }
+        cacheService.invalidate(email);
+    }
+
+    public TokenInfo login(MemberLoginRequest request) {
+        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword());
         Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
-        TokenInfo tokenInfo = jwtTokenProvider.generateToken(authentication);
-        return tokenInfo;
+        return jwtTokenProvider.generateToken(authentication);
+    }
+
+    public void checkIfExistingMember(String email) {
+        boolean existingEmail = memberRepository.existsByEmail(email);
+        if (existingEmail) {
+            throw new InvalidParameterException("Duplicated email exists.");
+        }
     }
 }
