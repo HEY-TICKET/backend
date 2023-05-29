87a32fd0a7412b6bb6de1d12231966964fb0b8f1
wonhee77 <leehi402@naver.com>
2023-05-30 02:06:51 +0900

    feat: GlobalExceptionHandler badCredentialException 추가

--- src/main/java/com/heyticket/backend/exception/GlobalExceptionHandler.java
+++ src/main/java/com/heyticket/backend/exception/GlobalExceptionHandler.java
@@ -4,6 +4,7 @@ import com.heyticket.backend.service.dto.response.CommonResponse;
 import java.util.NoSuchElementException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -12,17 +13,17 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(value = RuntimeException.class)
-    public ResponseEntity<?> runtimeExceptionHandler(Exception e) {
+    public ResponseEntity<?> runtimeExceptionHandler(RuntimeException e) {
         log.warn(e.getMessage());
         e.printStackTrace();
         return CommonResponse.serverError(InternalCode.SERVER_ERROR, e.getMessage());
     }
 
     @ExceptionHandler(value = NoSuchElementException.class)
-    public ResponseEntity<?> noSuchElementExceptionHandler(Exception e) {
+    public ResponseEntity<?> noSuchElementExceptionHandler(NoSuchElementException e) {
         log.warn(e.getMessage());
         e.printStackTrace();
-        return CommonResponse.notFound(InternalCode.SERVER_ERROR, e.getMessage());
+        return CommonResponse.notFound(InternalCode.NOT_FOUND, e.getMessage());
     }
 
     @ExceptionHandler(value = JwtValidationException.class)
@@ -30,5 +31,12 @@ public class GlobalExceptionHandler {
         log.warn(e.getMessage());
         e.printStackTrace();
         return CommonResponse.serverError(e.getCode(), e.getMessage());
+    }
+
+    @ExceptionHandler(value = BadCredentialsException.class)
+    public ResponseEntity<?> badCredentialExceptionHandler(BadCredentialsException e) {
+        log.warn("Invalid user information.");
+        e.printStackTrace();
+        return CommonResponse.badRequest(InternalCode.INVALID_JWT, "Invalid user information.");
     }
 }
