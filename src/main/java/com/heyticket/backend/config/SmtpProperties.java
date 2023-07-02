d20634e87dafe9ade6efd003384e1e97c4efb2ae
wonhee77 <leehi402@naver.com>
2023-07-02 13:38:15 +0900

    feat: smtp 관련 properties @ConfigurationProperties 빈으로 등록하도록 변경

---
+++ src/main/java/com/heyticket/backend/config/SmtpProperties.java
@@ -0,0 +1,53 @@
+package com.heyticket.backend.config;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@ConfigurationProperties(prefix = "mail")
+@Component
+@Getter
+@Setter
+public class SmtpProperties {
+
+    private Smtp smtp;
+
+    private String adminId;
+
+    private String adminPw;
+
+    @Getter
+    @Setter
+    public static class Smtp {
+
+        private boolean auth;
+
+        private StartTls starttls;
+
+        private SocketFactory socketFactory;
+
+        private int port;
+
+        @Getter
+        @Setter
+        public static class StartTls {
+
+            private boolean required;
+
+            private boolean enable;
+        }
+
+        @Getter
+        @Setter
+        public static class SocketFactory {
+
+            private String clazz;
+
+            private boolean fallback;
+
+            private int port;
+            
+        }
+    }
+}
