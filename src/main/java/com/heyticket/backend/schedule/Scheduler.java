127edfeb6d76a2c495c857135456f92e081250d6
wonhee77 <leehi402@naver.com>
2023-07-06 22:59:19 +0900

    feat: 공연 랭킹 업데이트 시간 오전 9시로 변경

--- src/main/java/com/heyticket/backend/schedule/Scheduler.java
+++ src/main/java/com/heyticket/backend/schedule/Scheduler.java
@@ -34,7 +34,7 @@ public class Scheduler {
         Executors.newFixedThreadPool(1);
     }
 
-    @Scheduled(cron = "0 0 6 * * *")
+    @Scheduled(cron = "0 0 9 * * *")
     public void updateBoxOfficeRank() {
         performanceService.updateBoxOfficeRankBatch();
     }
