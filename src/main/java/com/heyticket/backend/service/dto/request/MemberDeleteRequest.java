4d26822214905da4b1105f647b814abfc2b1de0f
wonhee77 <leehi402@naver.com>
2023-07-18 02:00:36 +0900

    feat: request dto 기본 생성자 추가

--- src/main/java/com/heyticket/backend/service/dto/request/MemberDeleteRequest.java
+++ src/main/java/com/heyticket/backend/service/dto/request/MemberDeleteRequest.java
@@ -1,13 +1,17 @@
 package com.heyticket.backend.service.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class MemberDeleteRequest {
 
     @NotBlank
