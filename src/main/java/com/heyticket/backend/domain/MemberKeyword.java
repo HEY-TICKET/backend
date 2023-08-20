d6c6eaa03ace33b769844d942479f5c659b82527
wonhee77 <leehi402@naver.com>
2023-08-21 00:45:50 +0900

    feat: MemberKeyword entity keyword 변수 타입 String 에서 Keyword entity로 변경

--- src/main/java/com/heyticket/backend/domain/MemberKeyword.java
+++ src/main/java/com/heyticket/backend/domain/MemberKeyword.java
@@ -1,6 +1,5 @@
 package com.heyticket.backend.domain;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
@@ -8,7 +7,6 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import java.util.Objects;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -25,39 +23,19 @@ public class MemberKeyword extends BaseTimeEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column
-    private String keyword;
-
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "email")
     private Member member;
 
-    public static MemberKeyword of(String keyword) {
-        return new MemberKeyword(keyword);
-    }
-
-    public MemberKeyword(String keyword) {
-        this.keyword = keyword;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-        MemberKeyword that = (MemberKeyword) o;
-        return Objects.equals(keyword, that.keyword);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(keyword);
-    }
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "keyword_id")
+    private Keyword keyword;
 
     public void setMember(Member member) {
         this.member = member;
+    }
+
+    public void setKeyword(Keyword keyword) {
+        this.keyword = keyword;
     }
 }
