6b3c5af3dd6dc6eccd9e518b600f8780b1b9d19a
wonhee77 <leehi402@naver.com>
2023-08-21 21:43:36 +0900

    test: Keyword 삭제 테스트 코드 구현

--- src/test/java/com/heyticket/backend/service/KeywordServiceTest.java
+++ src/test/java/com/heyticket/backend/service/KeywordServiceTest.java
@@ -3,11 +3,13 @@ package com.heyticket.backend.service;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import com.heyticket.backend.config.JpaConfig;
+import com.heyticket.backend.domain.Keyword;
 import com.heyticket.backend.domain.Member;
 import com.heyticket.backend.domain.MemberKeyword;
 import com.heyticket.backend.repository.keyword.KeywordRepository;
 import com.heyticket.backend.repository.member.MemberKeywordRepository;
 import com.heyticket.backend.repository.member.MemberRepository;
+import com.heyticket.backend.service.dto.request.KeywordDeleteRequest;
 import com.heyticket.backend.service.dto.request.KeywordSaveRequest;
 import java.util.ArrayList;
 import java.util.List;
@@ -53,25 +55,18 @@ public class KeywordServiceTest {
 
     @Test
     @DisplayName("Keyword 추가 - 새로운 키워드일 경우 키워드를 추가하고 연관관계를 맺는다")
-    void saveKeyword() {
+    void saveKeyword_newKeyword() {
         //given
-        Member member = Member.builder()
-            .email("email")
-            .password("encodedPassword")
-            .memberAreas(new ArrayList<>())
-            .memberGenres(new ArrayList<>())
-            .memberLikes(new ArrayList<>())
-            .memberKeywords(new ArrayList<>())
-            .build();
+        Member member = createMember();
 
         memberRepository.save(member);
 
+        //when
         KeywordSaveRequest request = KeywordSaveRequest.builder()
             .email(member.getEmail())
             .keyword("keyword")
             .build();
 
-        //when
         keywordService.saveKeyword(request);
 
         //then
@@ -80,5 +75,76 @@ public class KeywordServiceTest {
         MemberKeyword foundMemberKeyword = memberKeywords.get(0);
         assertThat(foundMemberKeyword.getMember().getEmail()).isEqualTo(member.getEmail());
         assertThat(foundMemberKeyword.getKeyword().getContent()).isEqualTo(request.getKeyword());
+    }
+
+    @Test
+    @DisplayName("Keyword 추가 - 기존에 등록된 키워드일 경우 연관관계를 맺는다")
+    void saveKeyword_keywordExist() {
+        //given
+        String content = "content";
+
+        Member member = createMember();
+
+        memberRepository.save(member);
+
+        Keyword keyword = Keyword.of(content);
+        keywordRepository.save(keyword);
+
+        //when
+        KeywordSaveRequest request = KeywordSaveRequest.builder()
+            .email(member.getEmail())
+            .keyword(content)
+            .build();
+
+        keywordService.saveKeyword(request);
+
+        //then
+        List<MemberKeyword> memberKeywords = memberKeywordRepository.findAll();
+        assertThat(memberKeywords).hasSize(1);
+        MemberKeyword foundMemberKeyword = memberKeywords.get(0);
+        assertThat(foundMemberKeyword.getMember().getEmail()).isEqualTo(member.getEmail());
+        assertThat(foundMemberKeyword.getKeyword().getContent()).isEqualTo(request.getKeyword());
+    }
+    
+    @Test
+    @DisplayName("Keyword 삭제 - keyword와 memberKeyword를 삭제한다")
+    void deleteKeyword(){
+        //given
+        Member member = createMember();
+        memberRepository.save(member);
+
+        Keyword keyword = Keyword.of("content");
+        keywordRepository.save(keyword);
+
+        MemberKeyword memberKeyword = MemberKeyword.builder()
+            .member(member)
+            .keyword(keyword)
+            .build();
+        memberKeywordRepository.save(memberKeyword);
+
+        //when
+        KeywordDeleteRequest request = KeywordDeleteRequest.builder()
+            .email(member.getEmail())
+            .keyword(keyword.getContent())
+            .build();
+
+        keywordService.deleteKeyword(request);
+        
+        //then
+        List<Keyword> foundKeywords = keywordRepository.findAll();
+        assertThat(foundKeywords).hasSize(0);
+        List<MemberKeyword> foundMemberKeywords = memberKeywordRepository.findAll();
+        assertThat(foundMemberKeywords).hasSize(0);
+    }
+
+    private Member createMember() {
+        return Member.builder()
+            .email("email")
+            .password("encodedPassword")
+            .memberAreas(new ArrayList<>())
+            .memberGenres(new ArrayList<>())
+            .memberLikes(new ArrayList<>())
+            .memberKeywords(new ArrayList<>())
+            .build();
     }
 }
